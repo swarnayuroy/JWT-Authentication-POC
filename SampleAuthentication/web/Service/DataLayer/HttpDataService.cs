ccdf95c9b1d908429d58b39d60f84518fecb87f0
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-01-31 11:56:15 +0530

    included HTTP data service

---
+++ SampleAuthentication/web/Service/DataLayer/HttpDataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using web.Models;
+
+namespace web.Service.DataLayer
+{
+    public class HttpDataService : IHttpService
+    {
+        private readonly HttpClient _client;
+        public HttpDataService() {
+            this._client = new HttpClient();
+            this._client.BaseAddress = new Uri(ConfigurationManager.AppSettings["serviceUri"]);
+            this._client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public Task<HttpResponseMessage> CheckCredential(Credential credential)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> RegisterUser(Registration detail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SampleAuthentication/web/Service/IHttpService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using web.Models;
+using System.Threading.Tasks;
+
+namespace web.Service
+{
+    public interface IHttpService
+    {
+        Task<HttpResponseMessage> CheckCredential(Credential credential);
+        Task<HttpResponseMessage> RegisterUser(Registration detail);
+    }
+}
