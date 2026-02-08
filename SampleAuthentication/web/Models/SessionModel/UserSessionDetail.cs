cbd87a4896f76e95e10e1c814f1ab01bb88b22c0
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-02-08 18:14:25 +0530

    included User Session class and worked on auth-board UI

---
+++ SampleAuthentication/web/Models/SessionModel/UserSessionDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.Models.SessionModel
+{
+    public class UserDetail 
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+    public class UserSessionDetail
+    {
+        private string _avatarText;
+        public string AvatarText
+        {
+            get
+            {
+                return _avatarText.ToUpper();
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    string[] nameArray = value.Split(' ');
+                    _avatarText = nameArray.Length > 1 ? $"{nameArray[0][0]}{nameArray[nameArray.Length - 1][0]}" : $"{nameArray[0][0]}";
+                }
+                else
+                {
+                    _avatarText = "?";
+                }
+            }
+        }
+        public UserDetail User { get; set; }
+        public ToastNotification ToastNotification { get; set; }
+    }
+}
