13dd3c059e7a8768764563a29986844f3b6ad52a
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-02-28 12:05:40 +0530

    included role property in the web

--- SampleAuthentication/web/Models/SessionModel/UserSessionDetail.cs
+++ SampleAuthentication/web/Models/SessionModel/UserSessionDetail.cs
@@ -10,6 +10,7 @@ namespace web.Models.SessionModel
         public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Role { get; set; }
         public bool? IsVerified { get; set; }
     }
     public class UserSessionDetail
