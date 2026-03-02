078b30a5bdd08072be2d14e9d75f0914a92042e5
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-03-02 21:39:07 +0530

    implemented no-content layout

--- SampleAuthentication/web/Controllers/HomeController.cs
+++ SampleAuthentication/web/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
