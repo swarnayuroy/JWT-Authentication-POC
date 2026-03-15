99d2100ac3305486ef55d3c7ff1575024dd6a727
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-03-15 12:33:14 +0530

    initial progress on angular-web

--- SampleAuthentication/angular-web/src/app/app.component.ts
+++ SampleAuthentication/angular-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AccountComponent } from './account/account.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AccountComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
