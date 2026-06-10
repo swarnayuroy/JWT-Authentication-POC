338976aef27ca822a9ee0c95daeb31692a448a1d
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-06-10 20:33:57 +0530

    included feature flag for further development of admin modal

---
+++ SampleAuthentication/web/Utils/FeatureFlag/FlagList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.Utils.FeatureFlag
+{
+    public enum AdminFlags
+    {
+        PRESENTATION,
+        CREATE,
+        EDIT,
+        DELETE
+    }
+
+    public class AdminFeatureFlag
+    {
+        private string _devType;
+        public AdminFlags Flag { get; set; }
+        public string DevType { get{ return _devType; } set{ _devType = value == "MODAL" ? value : null; } }
+        public bool IsEnabled { get; set; }
+    }
+
+    public class FlagList
+    {
+        public IList<AdminFeatureFlag> adminFeatureFlag = new List<AdminFeatureFlag>()
+        {
+            new AdminFeatureFlag() { Flag = AdminFlags.PRESENTATION, DevType = "MODAL", IsEnabled = false },
+            new AdminFeatureFlag() { Flag = AdminFlags.CREATE, DevType = "MODAL", IsEnabled = false },
+            new AdminFeatureFlag() { Flag = AdminFlags.EDIT, DevType = "MODAL", IsEnabled = false },
+            new AdminFeatureFlag() { Flag = AdminFlags.DELETE, DevType = "MODAL", IsEnabled = false }
+        };
+    }
+}
