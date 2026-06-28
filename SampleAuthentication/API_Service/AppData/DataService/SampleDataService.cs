5d62f108a177ea719e851fbb6f6e37726b86f45c
DESKTOP-6NRNP7L\HP <swarnayuroy@gmail.com>
2026-06-28 18:03:40 +0530

    refactoring of services

--- SampleAuthentication/API_Service/AppData/DataService/SampleDataService.cs
+++ SampleAuthentication/API_Service/AppData/DataService/SampleDataService.cs
@@ -13,23 +13,23 @@ namespace API_Service.AppData.DataService
 {
     public class Data
     {
-        private IEnumerable<Models.Entities.User> _user = Enumerable.Empty<Models.Entities.User>();
-        private IEnumerable<Models.Entities.Account> _usersAccount = Enumerable.Empty<Models.Entities.Account>();
+        private IEnumerable<UserDetail> _userDetails = Enumerable.Empty<UserDetail>();
+        private Models.Entities.Account _usersAccount = new Models.Entities.Account();
         private UserDetail _userDetail = new UserDetail();
         private FullUserDetail _fullUserDetail = new FullUserDetail();
 
-        public IEnumerable<Models.Entities.User> User
+        public IEnumerable<UserDetail> Users
         {
             get
             {
-                return _user;
+                return _userDetails;
             }
 
             set
             {
                 if (value.Any())
                 {
-                    _user = value;
+                    _userDetails = value;
                 }
             }
         }
@@ -58,7 +58,7 @@ namespace API_Service.AppData.DataService
             }
         }
 
-        public IEnumerable<Models.Entities.Account> AccountDetail
+        public Models.Entities.Account AccountDetail
         {
             get
             {
@@ -67,7 +67,7 @@ namespace API_Service.AppData.DataService
 
             set
             {
-                if (value.Any())
+                if (value!=null)
                 {
                     _usersAccount = value;
                 }
@@ -101,43 +101,43 @@ namespace API_Service.AppData.DataService
         public Task<IEnumerable<T>> Get()
         {
             var dataContext = new Data();
-            if (typeof(T) == typeof(Models.Entities.User))
-            {
-                var users = (from user in _dataContextProvider.User
-                             join userRole in _dataContextProvider.UserRole
-                             on user.Id equals userRole.UserId
-                             select new Models.Entities.User
-                             {
-                                 Id = user.Id, // Fix: assign Guid directly, not string
-                                 Name = user.Name,
-                                 Email = user.Email,
-                                 Role = userRole.Role == UserRoleType.Superadmin ? "Superadmin" : userRole.Role == UserRoleType.Admin ? "Admin" : "User",
-                                 IsVerified = user.IsVerified
-                             }).ToList<Models.Entities.User>();
+            //if (typeof(T) == typeof(Models.Entities.User))
+            //{
+            //    var users = (from user in _dataContextProvider.User
+            //                 join userRole in _dataContextProvider.UserRole
+            //                 on user.Id equals userRole.UserId
+            //                 select new Models.Entities.User
+            //                 {
+            //                     Id = user.Id, // Fix: assign Guid directly, not string
+            //                     Name = user.Name,
+            //                     Email = user.Email,
+            //                     Role = userRole.Role == UserRoleType.Superadmin ? "Superadmin" : userRole.Role == UserRoleType.Admin ? "Admin" : "User",
+            //                     IsVerified = user.IsVerified
+            //                 }).ToList<Models.Entities.User>();
 
-                _logger.LogDetails(LogType.INFO, $"Fetched {users.Count()} users from data provider.");
+            //    _logger.LogDetails(LogType.INFO, $"Fetched {users.Count()} users from data provider.");
 
-                dataContext.User = users;                
-                return Task.FromResult((IEnumerable<T>)dataContext.User!);
-            }
-            else if (typeof(T) == typeof(Models.Entities.Account))
-            {
-                var accountDetails = _dataContextProvider.Account.Select(a => new Models.Entities.Account
-                {
-                    Id = a.Id,
-                    UserId = a.UserId,
-                    Password = a.Password,
-                    CreatedAt = a.CreatedAt,
-                    LoggedInAt = a.LoggedInAt
-                });
-                _logger.LogDetails(LogType.INFO, $"Fetched {accountDetails.Count()} accounts from data provider.");
+            //    dataContext.User = users;                
+            //    return Task.FromResult((IEnumerable<T>)dataContext.User!);
+            //}
+            //else if (typeof(T) == typeof(Models.Entities.Account))
+            //{
+            //    var accountDetails = _dataContextProvider.Account.Select(a => new Models.Entities.Account
+            //    {
+            //        Id = a.Id,
+            //        UserId = a.UserId,
+            //        Password = a.Password,
+            //        CreatedAt = a.CreatedAt,
+            //        LoggedInAt = a.LoggedInAt
+            //    });
+            //    _logger.LogDetails(LogType.INFO, $"Fetched {accountDetails.Count()} accounts from data provider.");
 
-                dataContext.AccountDetail = accountDetails;
-                return Task.FromResult((IEnumerable<T>)dataContext.AccountDetail!);
-            }
+            //    dataContext.AccountDetail = accountDetails;
+            //    return Task.FromResult((IEnumerable<T>)dataContext.AccountDetail!);
+            //}
 
             _logger.LogDetails(LogType.WARNING, $"Type {typeof(T).Name} is not supported type");
-            throw new NotSupportedException($"Type {typeof(T).Name} is not supported type");
+            throw new NotFiniteNumberException($"Type {typeof(T).Name} is not supported type");
         }
 
         public async Task<bool> Save(T entity)
@@ -415,6 +415,7 @@ namespace API_Service.AppData.DataService
         {
             if (!string.IsNullOrEmpty(userType))
             {
+                var dataContext = new Data();
                 switch (userType)
                 {
                     case "Admin":
@@ -431,9 +432,21 @@ namespace API_Service.AppData.DataService
                                               Password = string.Empty
                                           }).ToList<UserDetail>();
 
-                        _logger.LogDetails(LogType.INFO, $"Fetched {adminUsers.Count()} admin users from data provider.");
-
-                        return Task.FromResult((IEnumerable<UserDetail>)adminUsers);
+                        if (adminUsers.Any())
+                        {
+                            if (adminUsers.Count() == 1)
+                            {
+                                _logger.LogDetails(LogType.INFO, $"Fetched 1 admin only.");
+                            }
+                            else
+                            {
+                                _logger.LogDetails(LogType.INFO, $"Fetched {adminUsers.Count()} admins.");
+                            }
+                            dataContext.Users = adminUsers;
+                            return Task.FromResult(dataContext.Users);
+                        }
+                        _logger.LogDetails(LogType.WARNING, $"No admin users found!");
+                        return Task.FromResult(Enumerable.Empty<UserDetail>());
 
                     case "User":
                         var users = (from user in _dataContextProvider.User
@@ -449,9 +462,20 @@ namespace API_Service.AppData.DataService
                                          Password = string.Empty
                                      }).ToList<UserDetail>();
 
-                        _logger.LogDetails(LogType.INFO, $"Fetched {users.Count()} admin users from data provider.");
-
-                        return Task.FromResult((IEnumerable<UserDetail>)users);
+                        if (users.Any())
+                        {
+                            if (users.Count()==1)
+                            {
+                                _logger.LogDetails(LogType.INFO, $"Fetched 1 user only.");
+                            }
+                            else
+                            {
+                                _logger.LogDetails(LogType.INFO, $"Fetched {users.Count()} users.");
+                            }
+                            return Task.FromResult((IEnumerable<UserDetail>)users);
+                        }
+                        _logger.LogDetails(LogType.WARNING, $"No users found!");
+                        return Task.FromResult(Enumerable.Empty<UserDetail>());
 
                     default:
                         _logger.LogDetails(LogType.WARNING, $"No users found of type {userType}");
@@ -466,6 +490,7 @@ namespace API_Service.AppData.DataService
         {
             if (!string.IsNullOrEmpty(searchTerm))
             {
+                var dataContext = new Data();
                 var userResults = (from user in _dataContextProvider.User
                                    join userRole in _dataContextProvider.UserRole on user.Id equals userRole.UserId
                                    where userRole.Role == UserRoleType.User &&
@@ -480,6 +505,22 @@ namespace API_Service.AppData.DataService
                                        IsVerified = user.IsVerified,
                                        Password = string.Empty
                                    }).ToList<UserDetail>();
+
+                if (userResults.Any())
+                {
+                    if (userResults.Count() == 1)
+                    {
+                        _logger.LogDetails(LogType.INFO, $"Fetched 1 user only for the search term: {searchTerm}.");
+                    }
+                    else
+                    {
+                        _logger.LogDetails(LogType.INFO, $"Fetched {userResults.Count()} users for the search term: {searchTerm}.");
+                    }
+                    dataContext.Users = userResults;
+                    return Task.FromResult(dataContext.Users);
+                }
+                _logger.LogDetails(LogType.WARNING, $"No users found for search term: {searchTerm}");
+                return Task.FromResult(Enumerable.Empty<UserDetail>());
             }
             _logger.LogDetails(LogType.WARNING, $"Couldn't generate result for empty search term");
             return Task.FromResult(Enumerable.Empty<UserDetail>());
@@ -499,6 +540,7 @@ namespace API_Service.AppData.DataService
         {
             if (!string.IsNullOrEmpty(userId) && !string.IsNullOrEmpty(password))
             {
+                var dataContext = new Data();
                 if (Guid.TryParse(userId, out Guid id))
                 {
                     var accountDetail = (from account in _dataContextProvider.Account 
@@ -514,7 +556,8 @@ namespace API_Service.AppData.DataService
                     if (accountDetail != null)
                     {
                         _logger.LogDetails(LogType.INFO, $"Account found for UserId: {userId}");
-                        return Task.FromResult(accountDetail);
+                        dataContext.AccountDetail = accountDetail;
+                        return Task.FromResult(dataContext.AccountDetail);
                     }
                 }
             }
@@ -528,6 +571,7 @@ namespace API_Service.AppData.DataService
             {
                 if (Guid.TryParse(id, out Guid accountId))
                 {
+                    var dataContext = new Data();
                     var accountDetail = (from account in _dataContextProvider.Account
                                          where account.Id == accountId
                                          select new Models.Entities.Account
@@ -541,7 +585,8 @@ namespace API_Service.AppData.DataService
                     if (accountDetail != null)
                     {
                         _logger.LogDetails(LogType.INFO, $"Account found");
-                        return Task.FromResult(accountDetail);
+                        dataContext.AccountDetail = accountDetail;
+                        return Task.FromResult(dataContext.AccountDetail);
                     }
                 }                
             }
